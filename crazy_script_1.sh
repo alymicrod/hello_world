4bf8cf37d0b5895fb6de2b3d6dc24f9802ff049a
alymicrod <30446122+alymicrod@users.noreply.github.com>
2017-07-25 19:45:11 +0000

    Add files via upload

---
+++ crazy_script_1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# This is a comment
+#list files
+ls
+
+# redirect the output of ls to "Malory"
+ls > Malory
+
+# Print a blank line
+echo " "
+
+#Print "catting Malory"
+echo "catting Malory"
+
+# Print the contents of the file "Malory"
+cat Malory
+
+# Print an integer
+echo "1234"
+
+#Print a word
+echo "word"
+
+#remove a something 
+rm crazy_script_1.sh
+
+# print something 
+echo "location"
+
+#print current directory
+pwd
+
